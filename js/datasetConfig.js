7433be6217bdc5fb9fad8316624efd00a5ee5d45
shiro1307 <133876079+shiro1307@users.noreply.github.com>
2026-04-22 10:19:01 +0530

    Removed unused cities in dropdown.

--- js/datasetConfig.js
+++ js/datasetConfig.js
@@ -28,25 +28,7 @@ export const CITY_DATASETS = [
     name: "New York, USA",
     graphPath: "./new_york.json",
     labelsPath: "./new_york.json",  // if labels are in same file, use same path
-  },
-  {
-    key: "city_1",
-    name: "City 1",
-    graphPath: "./data/city_1_graph.json",
-    labelsPath: "./data/city_1_labels.json",
-  },
-  {
-    key: "city_2",
-    name: "City 2",
-    graphPath: "./data/city_2_graph.json",
-    labelsPath: "./data/city_2_labels.json",
-  },
-  {
-    key: "city_3",
-    name: "City 3",
-    graphPath: "./data/city_3_graph.json",
-    labelsPath: "./data/city_3_labels.json",
-  },
+  }
 ];
 
 export const DEFAULT_CITY_KEY = "brasilia";
